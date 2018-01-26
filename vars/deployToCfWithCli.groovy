2cec57c123868bd57e196969fae92aeb539f2afd
Daniel Kurzynski <daniel.kurzynski@sap.com>
2018-01-26 11:12:20 +0100

    Remove copying CF destinations
    The SDK provides this via the destination service in recent versions, so we don't need it here anymore.

--- vars/deployToCfWithCli.groovy
+++ vars/deployToCfWithCli.groovy
@@ -1,7 +1,4 @@
-import com.cloudbees.groovy.cps.NonCPS
 import com.sap.cloud.sdk.s4hana.pipeline.*
-
-import java.util.regex.Matcher
 
 def call(Map parameters = [:]) {
 
@@ -16,7 +13,6 @@ def call(Map parameters = [:]) {
         List parameterKeys = ['dockerImage',
                               'smokeTestStatusCode',
                               'deploymentType',
-                              'environmentVariables',
                               'org',
                               'space',
                               'apiEndpoint',
@@ -44,17 +40,17 @@ def call(Map parameters = [:]) {
             withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: cfTarget.credentialsId, passwordVariable: 'CF_PASSWORD', usernameVariable: 'CF_USERNAME']]) {
                 cfTarget.setUsername(CF_USERNAME)
                 cfTarget.setPassword(BashUtils.escape(CF_PASSWORD))
-                deploy(configuration.dockerImage, configuration.deploymentType, cfTarget, configuration.smokeTestStatusCode, configuration.environmentVariables)
+                deploy(configuration.dockerImage, configuration.deploymentType, cfTarget, configuration.smokeTestStatusCode)
             }
         } else if (cfTarget.isUsernameAndPasswordDefined()) {
-            deploy(configuration.dockerImage, configuration.deploymentType, cfTarget, configuration.smokeTestStatusCode, configuration.environmentVariables)
+            deploy(configuration.dockerImage, configuration.deploymentType, cfTarget, configuration.smokeTestStatusCode)
         } else {
             throw new Exception("ERROR - EITHER SPECIFY credentialsId OR username and password")
         }
     }
 }
 
-private deploy(dockerImage, deploymentType, cfTarget, statusCode, environmentVariables) {
+private deploy(dockerImage, deploymentType, cfTarget, statusCode) {
     executeDockerNative(dockerImage: dockerImage) {
         lock("${cfTarget.apiEndpoint}/${cfTarget.org}/${cfTarget.space}/${cfTarget.appName}") {
             if (deploymentType == DeploymentType.BLUE_GREEN) {
@@ -65,7 +61,6 @@ private deploy(dockerImage, deploymentType, cfTarget, statusCode, environmentVar
                     sh "chmod +x ${smokeTestScript}"
 
                     sh "cf login -u ${cfTarget.username} -p ${cfTarget.password} -a ${cfTarget.apiEndpoint} -o ${cfTarget.org} -s ${cfTarget.space}"
-                    copyUserVariablesToManifest(cfTarget.appName, environmentVariables, cfTarget.manifest)
                     sh "cf blue-green-deploy ${cfTarget.appName} -f ${cfTarget.manifest} ${smokeTest}"
 
                     sh "cf logout"
@@ -75,39 +70,4 @@ private deploy(dockerImage, deploymentType, cfTarget, statusCode, environmentVar
             }
         }
     }
-}
-
-private copyUserVariablesToManifest(appName, variablesToKeep, manifest) {
-    if (doesAppExists(appName)) {
-        String environmentVariables = sh returnStdout: true, script: "cf env ${appName}"
-        Map userVariables = extractUserVariables(environmentVariables, variablesToKeep)
-        Map manifestMap = readYaml file: manifest
-        ManifestUpdater updater = new ManifestUpdater(manifestMap)
-        updater.addEnvironmentsVariables(userVariables)
-        sh "rm ${manifest}"
-        writeYaml file: manifest, data: updater.getManifest()
-    }
-}
-
-@NonCPS
-private extractUserVariables(environmentVariables, variablesToKeep) {
-    Map userVariables = [:]
-    Matcher userEnvSectionMatcher = (environmentVariables =~ /User-Provided:\n(.+\n)*/)
-    if (userEnvSectionMatcher.find()) {
-        String userEnv = userEnvSectionMatcher.group()
-        for (int i = 0; i < variablesToKeep.size(); i++) {
-            String userEnvKey = variablesToKeep[i]
-            Matcher userEnvMatcher = (userEnv =~ /${userEnvKey}:(.*)/)
-            if (userEnvMatcher.find()) {
-                String destinationEnvValue = userEnvMatcher.group(1)
-                userVariables[userEnvKey] = destinationEnvValue
-            }
-        }
-    }
-    return userVariables
-}
-
-private doesAppExists(String appName) {
-    String apps = sh returnStdout: true, script: 'cf apps'
-    return apps.contains(appName)
 }
