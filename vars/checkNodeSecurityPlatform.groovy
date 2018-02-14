a5bfd8f8952d07026af8df3c9c4687a0556d13fd
Ramachandra Kamath Arbettu <ramachandra.kamath.arbettu@sap.com>
2018-02-14 12:44:05 +0100

    Fix for change in Docker Pipeline Plugin

--- vars/checkNodeSecurityPlatform.groovy
+++ vars/checkNodeSecurityPlatform.groovy
@@ -1,8 +1,9 @@
+import com.sap.cloud.sdk.s4hana.pipeline.BashUtils
 def call(Map parameters = [:]) {
     handleStepErrors(stepName: 'checkNodeSecurityPlatform', stepParameters: parameters) {
         final script = parameters.script
         try {
-            executeNpm(script: script, dockerImage: 'allthings/nsp') {
+            executeNpm(script: script, dockerImage: 'allthings/nsp', dockerOptions: "--entrypoint=''") {
                 sh '''
                 set -o pipefail
                 nsp check 2>&1 | tee nsp.log
