02bf2fd50ad821fdd3ec62e2618dee5e2c6bbf13
Florian Wilhelm <florian.wilhelm02@sap.com>
2018-05-07 11:45:55 +0200

    Update SAP/jenkins-library dependency
    Included improvements:
    * Add configuration support for postActions
    * Explicitly state return types

--- vars/loadPiper.groovy
+++ vars/loadPiper.groovy
@@ -1,4 +1,4 @@
 def call(Map parameters = [:]) {
     // If you change the version please also update build.gradle and the corresponding jar file
-    library 'piper-library-os@da2f041a6d28a944f5c4648b1145c963fa087452'
+    library 'piper-library-os@e8fb41aab6386c7e3b668ff0e6f4a17d4c50ccb4'
 }
