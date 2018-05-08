71a1f895b1fe2a0ec34bcfe1650225e4c682fb42
Daniel Kurzynski <daniel.kurzynski@sap.com>
2018-05-08 11:10:03 +0200

    To not trigger any scripts when installing WhiteSource
    Without this change, the installation of WhiteSource would trigger scripts, (e.g. `postinstall`), which are defined in the project's `package.json`. This is not intended here, since WhiteSource is not a dependency of the project, but a utility program.

--- vars/executeWhitesourceScanNpm.groovy
+++ vars/executeWhitesourceScanNpm.groovy
@@ -26,7 +26,7 @@ def call(Map parameters = [:]) {
         try {
             executeNpm(script: script, dockerOptions: DownloadCacheUtils.downloadCacheNetworkParam()) {
                 sh """
-                npm install whitesource --save-dev
+                npm install whitesource --save-dev --ignore-scripts
                 alias whitesource='node node_modules/whitesource/bin/whitesource.js'
                 whitesource run
                 """.trim()
