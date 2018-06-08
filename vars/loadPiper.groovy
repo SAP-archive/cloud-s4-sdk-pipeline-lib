9fad305cdbc36a027f0d0a2adca77146d8eacf7b
Florian Wilhelm <florian.wilhelm02@sap.com>
2018-06-08 12:42:23 +0200

    Update Jenkins library for Maven logs without download info
    This version contains the "Disable download logs in Maven by default"
    change, which will make our logs easier to read and smaller. See
    https://github.com/SAP/jenkins-library/pull/159 for details.

--- vars/loadPiper.groovy
+++ vars/loadPiper.groovy
@@ -1,4 +1,4 @@
 def call(Map parameters = [:]) {
     // If you change the version please also update build.gradle and the corresponding jar file
-    library 'piper-library-os@e8fb41aab6386c7e3b668ff0e6f4a17d4c50ccb4'
+    library 'piper-library-os@6dc13801b0223b9d4ea93eb36098773edf504883'
 }
