7b5412edab5f530db64036afecef4f5b388a674f
Florian Wilhelm <florian.wilhelm02@sap.com>
2018-06-12 16:09:41 +0200

    Add option to skip zero downtime deployment for production
    This option should only be set if you can't use the other deployment types because of resource limits.

--- src/com/sap/cloud/sdk/s4hana/pipeline/DeploymentType.groovy
+++ src/com/sap/cloud/sdk/s4hana/pipeline/DeploymentType.groovy
@@ -3,14 +3,18 @@ package com.sap.cloud.sdk.s4hana.pipeline
 enum DeploymentType {
     ROLLING_UPDATE, BLUE_GREEN, STANDARD
 
-    static DeploymentType getDepolymentTypeForProduction(CloudPlatform cloudPlatform) {
-        switch (cloudPlatform) {
-            case CloudPlatform.NEO:
-                return ROLLING_UPDATE
-            case CloudPlatform.CLOUD_FOUNDRY:
-                return BLUE_GREEN
-            default:
-                throw new RuntimeException("Unknown cloud platform: ${cloudPlatform}")
+    static DeploymentType selectFor(CloudPlatform cloudPlatform, boolean isProduction, boolean forceDowntimeDeployment = false) {
+        if ((!isProduction) || forceDowntimeDeployment) {
+            return STANDARD
+        } else {
+            switch (cloudPlatform) {
+                case CloudPlatform.NEO:
+                    return ROLLING_UPDATE
+                case CloudPlatform.CLOUD_FOUNDRY:
+                    return BLUE_GREEN
+                default:
+                    throw new RuntimeException("Unknown cloud platform: ${cloudPlatform}")
+            }
         }
     }
 }
