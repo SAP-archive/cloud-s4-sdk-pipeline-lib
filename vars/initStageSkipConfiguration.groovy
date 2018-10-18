ad9a1b6be17b736042aab55251f4ca6b2d14db7b
Florian Wilhelm <florian.wilhelm02@sap.com>
2018-10-18 12:25:44 +0200

    Rename 'sipping' for better readability

--- vars/initStageSkipConfiguration.groovy
+++ vars/initStageSkipConfiguration.groovy
@@ -3,24 +3,24 @@ import com.sap.piper.ConfigurationLoader
 def call(Map parameters) {
     def script = parameters.script
 
-    script.commonPipelineEnvironment.configuration.skipping = [:]
+    script.commonPipelineEnvironment.configuration.runStage = [:]
 
     if (fileExists('package.json')) {
-        script.commonPipelineEnvironment.configuration.skipping.FRONT_END_BUILD = true
-        script.commonPipelineEnvironment.configuration.skipping.FRONT_END_TESTS = true
+        script.commonPipelineEnvironment.configuration.runStage.FRONT_END_BUILD = true
+        script.commonPipelineEnvironment.configuration.runStage.FRONT_END_TESTS = true
     }
-    if (ConfigurationLoader.stageConfiguration(script, 'endToEndTests') && script.commonPipelineEnvironment.configuration.skipping.FRONT_END_BUILD) {
-        script.commonPipelineEnvironment.configuration.skipping.E2E_TESTS = true
+    if (ConfigurationLoader.stageConfiguration(script, 'endToEndTests') && script.commonPipelineEnvironment.configuration.runStage.FRONT_END_BUILD) {
+        script.commonPipelineEnvironment.configuration.runStage.E2E_TESTS = true
     }
     if (ConfigurationLoader.stageConfiguration(script, 'performanceTests')) {
-        script.commonPipelineEnvironment.configuration.skipping.PERFORMANCE_TESTS = true
+        script.commonPipelineEnvironment.configuration.runStage.PERFORMANCE_TESTS = true
     }
 
-    if (script.commonPipelineEnvironment.configuration.skipping.E2E_TESTS || script.commonPipelineEnvironment.configuration.skipping.PERFORMANCE_TESTS) {
-        script.commonPipelineEnvironment.configuration.skipping.REMOTE_TESTS = true
+    if (script.commonPipelineEnvironment.configuration.runStage.E2E_TESTS || script.commonPipelineEnvironment.configuration.runStage.PERFORMANCE_TESTS) {
+        script.commonPipelineEnvironment.configuration.runStage.REMOTE_TESTS = true
     }
     if (ConfigurationLoader.stageConfiguration(script, 'checkmarxScan') && isProductiveBranch(script: script)) {
-        script.commonPipelineEnvironment.configuration.skipping.CHECKMARX_SCAN = true
+        script.commonPipelineEnvironment.configuration.runStage.CHECKMARX_SCAN = true
     }
 
     def projectInterceptorFile = "${s4SdkGlobals.projectExtensionsDirectory}/additionalTools.groovy"
@@ -29,48 +29,48 @@ def call(Map parameters) {
     if((fileExists(projectInterceptorFile) || fileExists(repositoryInterceptorFile))
         && isProductiveBranch(script: script)
         && ConfigurationLoader.stageConfiguration(script, 'additionalTools')){
-        script.commonPipelineEnvironment.configuration.skipping.ADDITIONAL_TOOLS = true
+        script.commonPipelineEnvironment.configuration.runStage.ADDITIONAL_TOOLS = true
     }
 
     boolean isWhitesourceConfigured =
         ConfigurationLoader.stageConfiguration(script, 'whitesourceScan')
 
     if (isProductiveBranch(script: script) && isWhitesourceConfigured) {
-        script.commonPipelineEnvironment.configuration.skipping.WHITESOURCE_SCAN = true
+        script.commonPipelineEnvironment.configuration.runStage.WHITESOURCE_SCAN = true
     }
 
     if (fileExists('package.json')) {
-        script.commonPipelineEnvironment.configuration.skipping.NODE_SECURITY_SCAN = true
+        script.commonPipelineEnvironment.configuration.runStage.NODE_SECURITY_SCAN = true
     }
 
     if (ConfigurationLoader.stageConfiguration(script, 'sourceClearScan').credentialsId) {
-        script.commonPipelineEnvironment.configuration.skipping.SOURCE_CLEAR_SCAN = true
+        script.commonPipelineEnvironment.configuration.runStage.SOURCE_CLEAR_SCAN = true
     }
 
     if (ConfigurationLoader.stageConfiguration(script, 'fortifyScan') && isProductiveBranch(script: script)) {
-        script.commonPipelineEnvironment.configuration.skipping.FORTIFY_SCAN = true
+        script.commonPipelineEnvironment.configuration.runStage.FORTIFY_SCAN = true
     }
 
-    if (script.commonPipelineEnvironment.configuration.skipping.CHECKMARX_SCAN
-        || script.commonPipelineEnvironment.configuration.skipping.WHITESOURCE_SCAN
-        || script.commonPipelineEnvironment.configuration.skipping.SOURCE_CLEAR_SCAN
-        || script.commonPipelineEnvironment.configuration.skipping.FORTIFY_SCAN
-        || script.commonPipelineEnvironment.configuration.skipping.ADDITIONAL_TOOLS) {
-        script.commonPipelineEnvironment.configuration.skipping.THIRD_PARTY_CHECKS = true
+    if (script.commonPipelineEnvironment.configuration.runStage.CHECKMARX_SCAN
+        || script.commonPipelineEnvironment.configuration.runStage.WHITESOURCE_SCAN
+        || script.commonPipelineEnvironment.configuration.runStage.SOURCE_CLEAR_SCAN
+        || script.commonPipelineEnvironment.configuration.runStage.FORTIFY_SCAN
+        || script.commonPipelineEnvironment.configuration.runStage.ADDITIONAL_TOOLS) {
+        script.commonPipelineEnvironment.configuration.runStage.THIRD_PARTY_CHECKS = true
     }
 
     Map productionDeploymentConfiguration = ConfigurationLoader.stageConfiguration(script, 'productionDeployment')
 
     if ((productionDeploymentConfiguration.cfTargets || productionDeploymentConfiguration.neoTargets) && isProductiveBranch(script: script)) {
-        script.commonPipelineEnvironment.configuration.skipping.PRODUCTION_DEPLOYMENT = true
+        script.commonPipelineEnvironment.configuration.runStage.PRODUCTION_DEPLOYMENT = true
     }
 
     if (ConfigurationLoader.stageConfiguration(script, 'artifactDeployment') && isProductiveBranch(script: script)) {
-        script.commonPipelineEnvironment.configuration.skipping.ARTIFACT_DEPLOYMENT = true
+        script.commonPipelineEnvironment.configuration.runStage.ARTIFACT_DEPLOYMENT = true
     }
 
     def sendNotification = ConfigurationLoader.postActionConfiguration(script, 'sendNotification')
     if (sendNotification?.enabled && (!sendNotification.skipFeatureBranches || isProductiveBranch(script: script))) {
-        script.commonPipelineEnvironment.configuration.skipping.SEND_NOTIFICATION = true
+        script.commonPipelineEnvironment.configuration.runStage.SEND_NOTIFICATION = true
     }
 }
