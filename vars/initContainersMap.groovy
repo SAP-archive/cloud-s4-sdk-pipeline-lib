094cdbfa29ed8419c7f3dea07924d06d1174f3e8
Ramachandra Kamath Arbettu <ramachandra.kamath.arbettu@sap.com>
2018-12-04 09:44:53 +0100

    Improve k8s stage
    * Improve K8S container mapping
    
    * Check container list instead of map

--- vars/initContainersMap.groovy
+++ vars/initContainersMap.groovy
@@ -10,36 +10,20 @@ def call(Map parameters = [:]) {
     }
 }
 
-@NonCPS
 Map getContainersMap(script) {
     Map containers = [:]
-    def stageToStepMapping = ['buildBackend'        : ['mavenExecute': 'mavenExecute'],
-                              'buildFrontend'       : ['executeNpm': 'executeNpm'],
-                              'staticCodeChecks'    : ['mavenExecute': 'mavenExecute'],
-                              'unitTests'           : ['mavenExecute': 'mavenExecute'],
-                              'integrationTests'    : ['mavenExecute': 'mavenExecute'],
-                              'frontendUnitTests'   : ['executeNpm': 'executeNpm'],
-                              'nodeSecurityPlatform': ['executeNpm': 'checkNodeSecurityPlatform'],
-                              'endToEndTests'       : ['mavenExecute': 'mavenExecute', 'executeNpm': 'executeNpm', 'cloudFoundryDeploy': 'cloudFoundryDeploy', 'deployToNeoWithCli': 'deployToNeoWithCli'],
-                              'performanceTests'    : ['mavenExecute': 'mavenExecute', 'checkJMeter': 'checkJMeter', 'cloudFoundryDeploy': 'cloudFoundryDeploy', 'deployToNeoWithCli': 'deployToNeoWithCli'],
-                              's4SdkQualityChecks'  : ['mavenExecute': 'mavenExecute'],
-                              'artifactDeployment'  : ['mavenExecute': 'mavenExecute'],
-                              'whitesourceScan'     : ['mavenExecute': 'mavenExecute', 'executeNpm': 'executeNpm'],
-                              'sourceClearScan'     : ['executeSourceClearScan': 'executeSourceClearScan'],
-                              'productionDeployment': ['mavenExecute': 'mavenExecute', 'executeNpm': 'executeNpm', 'cloudFoundryDeploy': 'cloudFoundryDeploy', 'deployToNeoWithCli': 'deployToNeoWithCli']
-
-    ]
-    stageToStepMapping.each { stageName, stepsMap -> containers[stageName] = getContainerForStage(script, stageName, stepsMap) }
+    def stageToStepMapping = readYaml(text: libraryResource('containers_map.yml')).containerMaps
+    stageToStepMapping.each { stageName, stepsList -> containers[stageName] = getContainerForStage(script, stageName, stepsList) }
     return containers
 }
 
 @NonCPS
-def getContainerForStage(script, stageName, Map stepsMap) {
+def getContainerForStage(script, stageName, List stepsList) {
     def containers = [:]
-    stepsMap.each { containerName, stepName ->
+    stepsList.each { stepName ->
         def imageName = getDockerImageNameForStep(script, stageName, stepName)
         if (imageName) {
-            containers[imageName] = containerName.toString().toLowerCase()
+            containers[imageName] = stepName.toString().toLowerCase()
         }
     }
     return containers
