e778bdfb0afc4f4275367ebbd6053aaad7652594
Florian Wilhelm <florian.wilhelm02@sap.com>
2018-12-12 12:41:38 +0100

    Remove NSP support

--- vars/stageNodeSecurityPlatform.groovy
+++
@@ -1,7 +0,0 @@
-def call(Map parameters = [:]) {
-    def stageName = 'nodeSecurityPlatform'
-    def script = parameters.script
-    runAsStage(stageName: stageName, script: script) {
-        checkNodeSecurityPlatform script: script
-    }
-}
