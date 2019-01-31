a48a9e7d2bdb4f998070ab4c326a775b1490f645
Daniel Kurzynski <daniel.kurzynski@sap.com>
2019-01-31 13:37:36 +0100

    Update piper to fix bug on first cloud foundry deployment

--- vars/loadPiper.groovy
+++ vars/loadPiper.groovy
@@ -1,4 +1,4 @@
 def call(Map parameters = [:]) {
     // If you change the version please also update build.gradle and the corresponding jar file
-    library 'piper-library-os@6e26d78047dd0287e7f263b432c5bec7b08ad74c'
+    library 'piper-library-os@def66f4ffa9e614eeb3747599a0be7ea9a108453'
 }
