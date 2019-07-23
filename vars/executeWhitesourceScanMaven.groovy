db2669e8087000fd48dacd8726a55d1fbe589651
Daniel Kurzynski <daniel.kurzynski@sap.com>
2019-07-23 13:35:25 +0200

    Hide whitesource user token

--- vars/executeWhitesourceScanMaven.groovy
+++ vars/executeWhitesourceScanMaven.groovy
@@ -9,31 +9,30 @@ def call(Map parameters = [:]) {
 
         try {
             withCredentials([string(credentialsId: parameters.credentialsId, variable: 'orgToken')]) {
-                List defines = [
-                    "-Dorg.whitesource.orgToken=${BashUtils.escape(orgToken)}",
-                    "-Dorg.whitesource.product=${BashUtils.escape(parameters.product)}",
-                    '-Dorg.whitesource.checkPolicies=true',
-                    '-Dorg.whitesource.failOnError=true'
-                ]
+                loadUserKey(parameters.whitesourceUserTokenCredentialsId){ userKey ->
+                    List defines = [
+                        "-Dorg.whitesource.orgToken=${BashUtils.escape(orgToken)}",
+                        "-Dorg.whitesource.product=${BashUtils.escape(parameters.product)}",
+                        '-Dorg.whitesource.checkPolicies=true',
+                        '-Dorg.whitesource.failOnError=true'
+                    ]
 
-                if (parameters.projectName) {
-                    defines.add("-Dorg.whitesource.aggregateProjectName=${BashUtils.escape(parameters.projectName)}")
-                    defines.add('-Dorg.whitesource.aggregateModules=true')
-                }
+                    if (parameters.projectName) {
+                        defines.add("-Dorg.whitesource.aggregateProjectName=${BashUtils.escape(parameters.projectName)}")
+                        defines.add('-Dorg.whitesource.aggregateModules=true')
+                    }
 
-                if (parameters.whitesourceUserTokenCredentialsId) {
-                    withCredentials([string(credentialsId: parameters.whitesourceUserTokenCredentialsId, variable: 'userKey')]) {
+                    if(userKey){
                         defines.add("-Dorg.whitesource.userKey=${BashUtils.escape(userKey)}")
                     }
+                    mavenExecute(
+                        script: script,
+                        m2Path: s4SdkGlobals.m2Directory,
+                        pomPath: pomPath,
+                        goals: 'org.whitesource:whitesource-maven-plugin:update',
+                        defines: defines.join(' ')
+                    )
                 }
-
-                mavenExecute(
-                    script: script,
-                    m2Path: s4SdkGlobals.m2Directory,
-                    pomPath: pomPath,
-                    goals: 'org.whitesource:whitesource-maven-plugin:update',
-                    defines: defines.join(' ')
-                )
             }
         } finally {
             String whiteSourceTarget = Paths.get(parameters.basePath, "application", "target", "site", "whitesource").toString()
@@ -42,5 +41,16 @@ def call(Map parameters = [:]) {
                          reportDir   : whiteSourceTarget,
                          reportFiles : 'index.html', reportName: "Whitesource Policy Check (${parameters.basePath})"])
         }
+    }
+}
+
+private loadUserKey(whitesourceUserTokenCredentialsId, body){
+    if(whitesourceUserTokenCredentialsId) {
+        withCredentials([string(credentialsId: whitesourceUserTokenCredentialsId, variable: 'userKey')]) {
+            body(userKey)
+        }
+    }
+    else {
+        body(null)
     }
 }
