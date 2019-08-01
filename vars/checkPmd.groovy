47d7417e2463c1b88fffc6dcb1070992621caa82
Daniel Kurzynski <daniel.kurzynski@sap.com>
2019-08-01 17:09:32 +0200

    Use pmd rules from sdk v3

--- vars/checkPmd.groovy
+++ vars/checkPmd.groovy
@@ -79,7 +79,7 @@ def executeMavenPMD(script, options, Map configuration, String pomPath) {
         flags: '--batch-mode',
         pomPath: pomPath,
         m2Path: s4SdkGlobals.m2Directory,
-        goals: "com.sap.cloud.s4hana.quality:pmd-plugin:RELEASE:pmd",
+        goals: "com.sap.cloud.sdk.quality:pmd-plugin:RELEASE:pmd",
         defines: options,
         dockerImage: configuration.dockerImage
     )
