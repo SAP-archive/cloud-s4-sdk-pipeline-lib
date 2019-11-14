1084a38ba8fd9d8a468da18b3c4009ff0d052dd2
Nabil <nabil.shafi@sap.com>
2019-11-14 10:07:07 +0100

    Warn when pipeline is running in a non-multibranch job

--- vars/initS4SdkPipelineLibrary.groovy
+++ vars/initS4SdkPipelineLibrary.groovy
@@ -1,3 +1,5 @@
+import static com.sap.cloud.sdk.s4hana.pipeline.EnvironmentAssertionUtils.assertPluginIsActive
+
 def call(Map parameters) {
     handleStepErrors(stepName: 'initS4SdkPipelineLibrary', stepParameters: parameters) {
         def script = parameters.script
@@ -13,5 +15,17 @@ def call(Map parameters) {
             checkDiskSpace script: script
         }
         setupDownloadCache script: script
+
+        if(!env.BRANCH_NAME){
+
+            String message = "Please use Multibranch Pipeline Job. \n" +
+                             "The pipeline is designed for the Multibranch Pipeline job type. It appears that it is running in a Singlebranch Pipeline. \n" +
+                             "Please refer to 'https://jenkins.io/doc/book/pipeline/multibranch/#creating-a-multibranch-pipeline' for information on how to setup a Multibranch Pipeline. \n"
+
+            assertPluginIsActive('badge')
+            addBadge(icon: "warning.gif", text: message)
+            createSummary(icon: "warning.gif", text: "<h2>Please use multi branch pipeline</h2>\n" + message)
+        }
+
     }
 }
