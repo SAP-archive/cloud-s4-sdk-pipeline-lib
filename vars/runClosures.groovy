1dc85ce9657ef3abac8aa27f8c2e08e2b5ffe03c
Daniel Kurzynski <daniel.kurzynski@sap.com>
2020-02-11 13:08:13 +0100

    Disable shuffle deployments

--- vars/runClosures.groovy
+++ vars/runClosures.groovy
@@ -4,7 +4,6 @@ def call(Map closures, script) {
             parallel closures
         } else {
             def closuresToRun = closures.values().asList()
-            Collections.shuffle(closuresToRun) // Shuffle the list so no one tries to rely on the order of execution
             for (int i = 0; i < closuresToRun.size(); i++) {
                 (closuresToRun[i] as Closure).call()
             }
