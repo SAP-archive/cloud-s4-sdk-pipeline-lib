68af55ff1a5418de3772639060ac392faf0785cf
Nabil Shafi <nabil.shafi@sap.com>
2020-02-27 12:47:43 +0100

    Validate pipeline config against schema
    We just print validation results for now as we cannot ensure we have a full schema of valid config available.

--- vars/initS4SdkPipelineLibrary.groovy
+++ vars/initS4SdkPipelineLibrary.groovy
@@ -1,13 +1,12 @@
+import groovy.json.JsonBuilder
 import static com.sap.cloud.sdk.s4hana.pipeline.EnvironmentAssertionUtils.assertPluginIsActive
 
 def call(Map parameters) {
     handleStepErrors(stepName: 'initS4SdkPipelineLibrary', stepParameters: parameters) {
         def script = parameters.script
-
         if (!parameters.configFile) {
             parameters.configFile = getConfigLocation(script: script)
         }
-
         parameters.customDefaults = parameters.customDefaults ?: ['default_s4_pipeline_environment.yml']
         setupCommonPipelineEnvironment(parameters)
         convertLegacyConfiguration script: script
@@ -16,16 +15,25 @@ def call(Map parameters) {
         }
         setupDownloadCache script: script
 
-        if(!env.BRANCH_NAME){
+        if (!env.BRANCH_NAME) {
 
             String message = "Please use Multibranch Pipeline Job. \n" +
-                             "The pipeline is designed for the Multibranch Pipeline job type. It appears that it is running in a Singlebranch Pipeline. \n" +
-                             "Please refer to 'https://jenkins.io/doc/book/pipeline/multibranch/#creating-a-multibranch-pipeline' for information on how to setup a Multibranch Pipeline. \n"
+                "The pipeline is designed for the Multibranch Pipeline job type. It appears that it is running in a Singlebranch Pipeline. \n" +
+                "Please refer to 'https://jenkins.io/doc/book/pipeline/multibranch/#creating-a-multibranch-pipeline' for information on how to setup a Multibranch Pipeline. \n"
 
             assertPluginIsActive('badge')
             addBadge(icon: "warning.gif", text: message)
             createSummary(icon: "warning.gif", text: "<h2>Please use multi branch pipeline</h2>\n" + message)
         }
 
+        def configFileContent = readYaml file: parameters.configFile
+        def configFileAsJson = new JsonBuilder(configFileContent).toPrettyString()
+        writeFile file: 'pipeline_config.json', text: configFileAsJson
+        int status = sh(script: "curl --location --remote-name \"https://raw.githubusercontent.com/SchemaStore/schemastore/master/src/schemas/json/cloud-sdk-pipeline-config-schema.json\"", returnStatus: true)
+        if (status == 0 && !configFileContent.general.extensionRepository) {
+            executeNpm(script: script, defaultNpmRegistry: 'https://registry.npmjs.org/') {
+                sh(script: "npx ajv-cli validate -s 'cloud-sdk-pipeline-config-schema.json' -d 'pipeline_config.json' --all-errors", returnStatus: true)
+            }
+        }
     }
 }
