b668c9176b9e32f44c3dbb33145ca58dd4412a36
Daniel Kurzynski <daniel.kurzynski@sap.com>
2020-02-27 13:08:11 +0100

    Null-safe access

--- vars/initS4SdkPipelineLibrary.groovy
+++ vars/initS4SdkPipelineLibrary.groovy
@@ -30,7 +30,7 @@ def call(Map parameters) {
         def configFileAsJson = new JsonBuilder(configFileContent).toPrettyString()
         writeFile file: 'pipeline_config.json', text: configFileAsJson
         int status = sh(script: "curl --location --remote-name \"https://raw.githubusercontent.com/SchemaStore/schemastore/master/src/schemas/json/cloud-sdk-pipeline-config-schema.json\"", returnStatus: true)
-        if (status == 0 && !configFileContent.general.extensionRepository) {
+        if (status == 0 && !configFileContent.general?.extensionRepository) {
             executeNpm(script: script, defaultNpmRegistry: 'https://registry.npmjs.org/') {
                 sh(script: "npx ajv-cli validate -s 'cloud-sdk-pipeline-config-schema.json' -d 'pipeline_config.json' --all-errors", returnStatus: true)
             }
