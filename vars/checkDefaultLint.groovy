a8da49d8e8cf403ddea27605eded2bd642bde6a7
Kevin Hudemann <kevin.hudemann@sap.com>
2020-04-09 15:59:58 +0200

    Allow user to define a ci-lint script which is executed as part of the lint stage
    This change allows the user to provide a custom linting script to be
    executed as part of the lint stage of the pipeline. In addition, the
    default behavior of the lint stage is extended to not only provide the
    possibility to run the UI5 lint step for UI5 projects, but also provide
    an execution of ESLint with a provided default configuration for non-SAPUI5
    Javascript/Typescript projects.
    
    Previously, the lint stage just executed the UI5 lint step in case a SAPUI5
    project could be identified or the stage was skipped otherwise. The only
    way to run a custom linter was to make use of the extension mechanism.
    Now it is possible to run a custom linter by defining a script named
    'ci-lint' in the 'scripts' section in the 'package.json' file or files of the 
    project. In addition, the output of the linting tool is expected to be in
    checkstyle XML format and the file is name '*cilint.xml', which may reside
    in any directory of the project.
    
    If no ci-lint script is defined and the project is not a SAP UI5 project,
    ESLint is executed with a default configuration, which is applicable to
    a variety of Javascript/Typescript projects.

---
+++ vars/checkDefaultLint.groovy
@@ -0,0 +1,62 @@
+import com.sap.cloud.sdk.s4hana.pipeline.DownloadCacheUtils
+import com.sap.cloud.sdk.s4hana.pipeline.WarningsUtils
+
+import static com.sap.cloud.sdk.s4hana.pipeline.EnvironmentAssertionUtils.assertPluginIsActive
+
+void call(Map parameters = [:]) {
+
+    handleStepErrors(stepName: 'checkDefaultLint', stepParameters: parameters) {
+        final script = parameters.script
+        final Map configuration = parameters.configuration
+
+        Map failThreshold = [:]
+        List eslintConfigs = []
+
+        List dockerOptions = []
+        DownloadCacheUtils.appendDownloadCacheNetworkOption(script, dockerOptions)
+
+        try {
+            eslintConfigs = findFiles(glob: '**/.eslintrc*', excludes: '**/node_modules/**')
+        } catch (IOException ioe) {
+            error "An error occurred when looking for eslint config.\n"
+        }
+        int status
+        executeNpm(script: script, dockerImage: configuration?.dockerImage, dockerOptions: dockerOptions) {
+            // npm install seems to be necessary, since ESLint is not able to find plugins installed by npx on the fly
+            // and placed in npm cache
+            status = sh(script: "npm install eslint@latest typescript@latest @typescript-eslint/parser@latest @typescript-eslint/eslint-plugin@latest", returnStatus: true)
+
+            // If the user has ESLint configs in the project we use them to lint existing JS files. In this case we do not lint other types of files,
+            // i.e., .jsx, .ts, .tsx, since we can not be sure that the provided config enables parsing of these file types.
+            if (eslintConfigs.size() > 0){
+                for (int i = 0; i < eslintConfigs.size(); i++) {
+                    String eslintConfigPath = eslintConfigs[i].path.minus(eslintConfigs[i].name)
+
+                    // If the eslintConfigPath is empty, then ESLint config resides in root directory of the project
+                    if (!eslintConfigPath){
+                        // For the execution we rely on ESLint's built-in mechanism to determine which configuration to use for which JS files
+                        // More information on that can be found here: https://eslint.org/docs/user-guide/configuring#configuration-cascading-and-hierarchy
+                        status = sh(script: "npx --no-install eslint . -f checkstyle -o ./${i}_defaultlint.xml --ignore-pattern node_modules/", returnStatus: true)
+                    } else {
+                        status = sh(script: "npx --no-install eslint ${eslintConfigPath}**/*.js -f checkstyle -o ./${i}_defaultlint.xml --ignore-pattern node_modules/", returnStatus: true)
+                    }
+
+                    // From ESLint docs: Exit codes: 2: Linting was unsuccessful due to a configuration problem or an internal error.
+                    if (status == 2) {
+                        echo "ESLint returned exit code 2. There might be a problem with your ESLint configuration or ESLint had an internal error."
+                        echo "Please ensure that your ESLint configuration is correct. If the error persists, please file a ticket at: https://github.com/SAP/cloud-s4-sdk-pipeline/issues/new/choose"
+                    }
+                }
+            } else {
+                String eslintConfigDefault = libraryResource ".eslintrc.json"
+                writeFile file: ".eslintrc.json", text: eslintConfigDefault
+
+                // Here we can use the --ext .js,.jsx,.ts,.tsx option, since our general purpose config provides the necessary configuration to parse and process all kinds of JS/TS files
+                status = sh(script: "npx --no-install eslint . --ext .js,.jsx,.ts,.tsx -c ./.eslintrc.json -f checkstyle -o ./defaultlint.xml --ignore-pattern node_modules/", returnStatus: true)
+            }
+        }
+        executeWithLockedCurrentBuildResult(script: script, errorStatus: 'FAILURE', errorHandler: script.buildFailureReason.setFailureReason, errorHandlerParameter: 'Linter', errorMessage: "ESLint failed. Please fix the reported violations.") {
+            WarningsUtils.createLintingResultsReport(script, "defaultLint", "Linter", "*defaultlint.xml", failThreshold)
+        }
+    }
+}
