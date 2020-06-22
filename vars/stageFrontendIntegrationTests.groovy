30ffd3704ac595255cebd94440a49e8761c2fda0
Daniel Kurzynski <daniel.kurzynski@sap.com>
2020-06-22 19:56:47 +0200

    Use piper integration stage to run frontend integration tests

--- vars/stageFrontendIntegrationTests.groovy
+++ vars/stageFrontendIntegrationTests.groovy
@@ -1,39 +1,5 @@
-import com.sap.cloud.sdk.s4hana.pipeline.DownloadCacheUtils
-import com.sap.cloud.sdk.s4hana.pipeline.QualityCheck
-import com.sap.cloud.sdk.s4hana.pipeline.ReportAggregator
-import com.sap.piper.ConfigurationLoader
-
 def call(Map parameters = [:]) {
-    def stageName = 'frontendIntegrationTests'
     def script = parameters.script
-    piperStageWrapper(stageName: stageName, script: script) {
-        Map stageConfiguration = loadEffectiveStageConfiguration(script: script, stageName: stageName)
-        executeFrontendIntegrationTest(script, stageConfiguration)
-    }
-}
-
-private void executeFrontendIntegrationTest(def script, Map stageConfiguration) {
-    String name = 'Frontend Integration Tests'
-    String pattern = 's4hana_pipeline/reports/frontend-integration/**/*.xml'
-
-    def dockerOptions = ['--cap-add=SYS_ADMIN']
-    DownloadCacheUtils.appendDownloadCacheNetworkOption(script, dockerOptions)
-
-    try {
-        collectJUnitResults(script: script, testCategoryName: name, reportLocationPattern: pattern) {
-        executeNpm(script: script, dockerImage: stageConfiguration?.dockerImage, dockerOptions: dockerOptions) {
-            sh "Xvfb -ac :99 -screen 0 1280x1024x16 &"
-            withEnv(['DISPLAY=:99']) {
-                runOverNpmModules(script: script, npmScripts: ['ci-it-frontend']) { basePath ->
-                    dir(basePath) {
-                        sh "npm run ci-it-frontend"
-                    }
-                }
-            }
-        }
-        ReportAggregator.instance.reportTestExecution(QualityCheck.FrontendIntegrationTests)
-        }
-    } finally {
-        archiveArtifacts artifacts: 's4hana_pipeline/reports/frontend-integration/**', allowEmptyArchive: true
-    }
+    def stageName = 'frontendIntegrationTests'
+    piperPipelineStageIntegration script: script, stageName: stageName, npmExecuteScripts: true
 }
