0786c6cbb838ef59d778d66dea7407cf1ec8f887
Stephan Assmus <stephan.assmus@sap.com>
2020-09-01 16:54:57 +0200

    Restore unstashPiperBinInNonReleaseVersions

--- vars/loadPiper.groovy
+++ vars/loadPiper.groovy
@@ -23,7 +23,7 @@ def call(Map parameters = [:]) {
     library "${piperIdentifier}@${piperOsVersion}"
     Analytics.instance.setPiperIdentifier(piperIdentifier)
 
-//    unstashPiperBinInNonReleaseVersions(piperVersion: piperOsVersion)
+    unstashPiperBinInNonReleaseVersions(piperVersion: piperOsVersion)
 }
 
 private boolean isLibraryConfigured(String libName){
