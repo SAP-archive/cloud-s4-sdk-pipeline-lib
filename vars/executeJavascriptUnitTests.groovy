2a07754b4cd2d778bb64cfb149a593e4a687b665
Kevin Hudemann <kevin.hudemann@sap.com>
2020-09-25 11:02:38 +0200

    Migrate legacy steps to npmExecuteScripts (#745)

--- vars/executeJavascriptUnitTests.groovy
+++ vars/executeJavascriptUnitTests.groovy
@@ -1,17 +1,10 @@
-// keep because it is used in extensions repo
+// keep because it is used by legacy consumers
 def call(Map parameters) {
 
     Script script = parameters.script
     String name = 'Backend Unit Tests'
     String reportLocationPattern = 's4hana_pipeline/reports/backend-unit/**'
 
-    runOverNpmModules(script: script, npmScripts: ['ci-backend-unit-test']) { basePath ->
-        collectJUnitResults(script: script, testCategoryName: name, reportLocationPattern: reportLocationPattern) {
-            executeNpm(script: script, dockerOptions: []) {
-                dir(basePath) {
-                    sh "npm run ci-backend-unit-test"
-                }
-            }
-        }
-    }
+    npmExecuteScripts script: script, runScripts: ['ci-backend-unit-test']
+    testsPublishResults script: script, junit: [active: true, pattern: reportLocationPattern, allowEmptyResults: true]
 }
