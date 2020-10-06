a55de078a1aa3cf672460dfe27bc0675152abc47
Daniel Kurzynski <daniel.kurzynski@sap.com>
2020-10-06 11:08:51 +0200

    Infer projectName for locking

--- vars/stagePerformanceTests.groovy
+++ vars/stagePerformanceTests.groovy
@@ -8,21 +8,19 @@ def call(Map parameters = [:]) {
         final Map stageConfiguration = loadEffectiveStageConfiguration(script: script, stageName: stageName)
 
         if (stageConfiguration) {
-            lock(script.commonPipelineEnvironment.configuration.performanceTestLock) {
-                multicloudDeploy(
-                    script: script,
-                    stage: stageName
-                )
-                def jMeterConfig = ConfigurationLoader.stepConfiguration(script, 'checkJMeter')
-                if (jMeterConfig && jMeterConfig.enabled != false) {
-                    def performanceTestReports = new File("${workspace}/${script.s4SdkGlobals.performanceReports}/JMeter")
-                    performanceTestReports.mkdirs()
-                    checkJMeter script: script, reportDirectory: performanceTestReports
-                }
+            multicloudDeploy(
+                script: script,
+                stage: stageName
+            )
+            def jMeterConfig = ConfigurationLoader.stepConfiguration(script, 'checkJMeter')
+            if (jMeterConfig && jMeterConfig.enabled != false) {
+                def performanceTestReports = new File("${workspace}/${script.s4SdkGlobals.performanceReports}/JMeter")
+                performanceTestReports.mkdirs()
+                checkJMeter script: script, reportDirectory: performanceTestReports
+            }
 
-                if (ConfigurationLoader.stepConfiguration(script, 'checkGatling')?.enabled) {
-                    checkGatling script: script, appUrls: stageConfiguration.appUrls
-                }
+            if (ConfigurationLoader.stepConfiguration(script, 'checkGatling')?.enabled) {
+                checkGatling script: script, appUrls: stageConfiguration.appUrls
             }
         } else {
             echo "Performance tests have not been enabled. Skipping the stage."
