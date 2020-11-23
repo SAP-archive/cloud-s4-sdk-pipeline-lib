0ab7fe25fba99317e0b9755596b2ea7fcd0c2d57
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-11-23 15:08:45 +0100

    update

--- vars/cloudSdkPipeline.groovy
+++ vars/cloudSdkPipeline.groovy
@@ -1,9 +1,7 @@
 void call(parameters) {
-    node {
-        error '''
-        The SAP Cloud SDK Pipeline has been merged into General Purpose Pipeline of project "Piper".
-        The last available version is https://github.com/SAP/cloud-s4-sdk-pipeline/releases/tag/v45
-        Please see https://github.com/SAP/cloud-s4-sdk-pipeline/blob/master/gpp-guide.md for details on how to switch to General Purpose Pipeline.
-        '''
-    }
+    error '''
+    The SAP Cloud SDK Pipeline has been merged into General Purpose Pipeline of project "Piper".
+    The last available version is https://github.com/SAP/cloud-s4-sdk-pipeline/releases/tag/v45
+    Please see https://github.com/SAP/cloud-s4-sdk-pipeline/blob/master/gpp-guide.md for details on how to switch to General Purpose Pipeline.
+    '''
 }
