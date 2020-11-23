9095ce459e59726cdf013fbf0ab10969daf18e6b
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-11-23 14:34:57 +0100

    unset

--- vars/isProductiveBranch.groovy
+++
@@ -1,5 +0,0 @@
-def call(Map parameters) {
-    def script = parameters.script
-    Map configWithDefault = loadEffectiveGeneralConfiguration script: script
-    return configWithDefault['productiveBranch'] == env.BRANCH_NAME
-}
